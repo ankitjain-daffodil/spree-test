e1a4dd592ae55556b74761d053273d8a195f6587
Ankit Jain <ankit.jain@daffodilsw.com>
2017-12-22 15:59:19 +0530

    snagging

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 2 }
-end
+if Rails.env.production?
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 20 }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDIS_URL'], size: 2 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'], size: 20 }
+
+  end
 end
