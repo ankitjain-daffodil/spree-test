c66e478a301407064ed4878075be93e270185e3e
Ankit Jain <ankit.jain@daffodilsw.com>
2017-12-27 13:10:47 +0530

    add changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     UserNotifierMailer.send_signup_email().deliver_later
+    HardWorker.perform_async('bob', 5)
     render json: {test: 'hello'}
   end
 end
